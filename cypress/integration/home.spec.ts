191be40cef681eab91ac0c91642f7f07f7f78c11
Kerwin Yen <kevinyenhaha@gmail.com>
2022-11-22 18:54:54 +1100

    v1.1: completed task 1
    1. Completed 4. How to test multiple pages, 5. How to test user journeys

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,30 +1,35 @@
-import coursesJson from "../../data/courses.json"
-const { _ } = Cypress
-
-describe("RWT Home", function () {
-  beforeEach(function () {
-    cy.visit("/")
-  })
-
-  it("renders all of the course titles, descriptions & lessons", function () {
-    const courses = Object.keys(coursesJson)
-
-    _.each(courses, (course, index) => {
-      const title = coursesJson[course].title
-      const description = coursesJson[course].description
-      const lessons = coursesJson[course].lessons
-
-      cy.getBySel(`course-${index}`).within(($course) => {
-        cy.getBySel("course-title").contains(title)
-        cy.getBySel("course-description").contains(description)
-
-        _.each(lessons, (lesson, index) => {
-          const lessonTitle = lessons[index].title
-          cy.getBySel(`lesson-${index}`).contains(lessonTitle)
-        })
+describe("Home page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000")
+    })
+  
+    context("Hero section", () => {
+      it("the h1 contains the correct text", () => {
+        cy.getByData("hero-heading").contains(
+          "Testing Next.js Applications with Cypress"
+        )
+      })
+  
+      it("the features on the homepage are correct", () => {
+        cy.get("dt").eq(0).contains("4 Courses")
+        cy.get("dt").eq(1).contains("25+ Lessons")
+        cy.get("dt").eq(2).contains("Free and Open Source")
       })
     })
+  
+    context("Courses section", () => {
+        it("Course: Testing Your First Next.js Application", () => {
+            cy.getByData("course-0").find("a").eq(3).click()
+            cy.location("pathname").should("eq", "/testing-your-first-application")
+          })
+        it("Course: Testing Foundations", () => {
+            cy.getByData("course-1").find("a").eq(3).click()
+            cy.location("pathname").should("eq", "/testing-foundations")
+          })
+          it("Course: Cypress Fundamentals", () => {
+            cy.getByData("course-2").find("a").eq(3).click()
+            cy.location("pathname").should("eq", "/cypress-fundamentals")
+          })
+    })
   })
-})
-
-export {}
+  
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,5 @@
-/// <reference path="../global.d.ts" />
-// @ts-nocheck
-Cypress.Commands.add("getBySel", (selector, ...args) => {
-  return cy.get(`[data-test=${selector}]`, ...args)
-})
+/// <reference types="cypress" />
 
-Cypress.Commands.add("getBySelLike", (selector, ...args) => {
-  return cy.get(`[data-test*=${selector}]`, ...args)
-})
-
-let LOCAL_STORAGE_MEMORY = {}
-
-Cypress.Commands.add("saveLocalStorage", () => {
-  Object.keys(localStorage).forEach((key) => {
-    LOCAL_STORAGE_MEMORY[key] = localStorage[key]
+Cypress.Commands.add("getByData", (selector) => {
+    return cy.get(`[data-test=${selector}]`)
   })
-})
-
-Cypress.Commands.add("restoreLocalStorage", () => {
-  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
-    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key])
-  })
-})
-
-export {}
